fa97d023bf836d66bbfd7d6df58fa84f6285616f
ViezeFredsel <wngeck@avans.nl>
2015-12-10 17:17:11 +0100

    Interfaces Gemaakt

---
+++ Windows_XNA_Tanks/Windows_XNA_Tanks/Windows_XNA_Tanks/Interfaces/ITurret.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Windows_XNA_Tanks.Interfaces
+{
+    interface ITurret
+    {
+        void Shoot();
+        void Reload();
+        void TurnLeft();
+        void TurnRight();
+    }
+}
