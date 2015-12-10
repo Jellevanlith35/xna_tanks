d9956763ea9dd2691682efa2c9d2cf81c27f7d07
ViezeFredsel <wngeck@avans.nl>
2015-12-10 17:17:48 +0100

    Begin aan de Turret klasse

---
+++ Windows_XNA_Tanks/Windows_XNA_Tanks/Windows_XNA_Tanks/Model/Turrets/StandaardTurret.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows_XNA_Tanks.Interfaces;
+
+namespace Windows_XNA_Tanks.Model.Turrets
+{
+    public abstract class StandaardTurret : ITurret
+    {
+
+        private Texture2D _texture;
+        private Rectangle _rectangle;
+        private Vector2 _positionOnTank;
+        private float _rotation;
+        private Vector2 _origin;
+
+        public void StandaardTurret()
+        {
+            _rectangle = new Rectangle((int)_positionOnTank.X, (int)_positionOnTank.Y, _texture.Width, _texture.Height);
+            _origin = new Vector2(_rectangle.Width / 2, _rectangle.Height / 2);
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(_texture, _positionOnTank, null, Color.White, _rotation, _origin, 1f, SpriteEffects.None, 0);
+        }
+
+        public void Shoot()
+        {
+
+        }
+        public void Reload()
+        {
+
+        }
+        public void TurnLeft()
+        {
+            _rotation -= 0.02f;
+        }
+        public void TurnRight()
+        {
+            _rotation += 0.02f;
+        }
+
+        public Vector2 PositionOnTank
+        {
+            get { return _positionOnTank; }
+            set { _positionOnTank = value; }
+        }
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+            set { _texture = value; }
+        }
+    }
+}
